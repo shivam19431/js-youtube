d73105cbf4744d4a3c0628bbe4a6a128450b5ea3
Shivam Kumar <160920667+shivam19431@users.noreply.github.com>
2024-12-01 22:55:49 +0530

    basics_added
    new commit

---
+++ 01_DataOperations.js
@@ -0,0 +1,22 @@
+let score= undefined
+console.log(score)
+console.log(typeof score)
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+
+// "33abc"=>NaN
+// null(which is an object type)=>0
+// undefined=>NaN
+let loged=""
+let isbool=Boolean(loged)
+console.log(isbool)
+
+let pull=67
+let isString=String(pull)
+console.log(isString)
+console.log(typeof isString)
+
+//1=>true   0=>false
+//""=>false   "value"=>true
